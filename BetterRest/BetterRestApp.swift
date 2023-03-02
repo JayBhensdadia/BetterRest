32fc75d2f3de64720e641a339d0b17c42337a459
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-02 21:41:47 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jay Bhensdadia on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
