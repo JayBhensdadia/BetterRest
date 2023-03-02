bf19abc42831dbf1841478d0fe08e6c3067c0071
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-02 23:21:43 +0530

    removing calculate button and some styling changes

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,13 +14,22 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
+    
+    @State private var alertTitle = "Error"
+    @State private var alertMessage = "Sorry, there was a problem calculating your bedtime."
     @State private var showingAlert = false
+    
     
     static var defaultWakeTime : Date{
         var components = DateComponents()
         components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+   
+    static var defaultBedTime : Date{
+        var components = DateComponents()
+        components.hour = 10
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
     }
@@ -36,7 +45,35 @@ struct ContentView: View {
         return str
     }
     
+    
+    var recomendedBedTime : String{
+        
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let str = sleepTime.formatted(date: .omitted, time: .shortened)
+            return str
+            
+        }catch{
+            
+            showingAlert = true
+        }
+        
+        return ContentView.defaultBedTime.formatted(date:.omitted, time: .shortened)
+    }
+    
+    
     var body: some View {
+        
         NavigationStack{
             Form{
                 
@@ -47,6 +84,7 @@ struct ContentView: View {
                 }header: {
                     Text("WakeUp Time ⏰ ?")
                         .font(.headline)
+                        .fontWeight(.bold)
                 }
                 
                 Section{
@@ -72,11 +110,25 @@ struct ContentView: View {
                     
                 }
                 
+                
+                Section{
+                    Text(recomendedBedTime)
+                        //.foregroundColor(.secondary)
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .padding()
+                }header: {
+                    Text("Ideal Bed time")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                }
+              
+                
+                
+                
             }
             .navigationTitle("Better Rest")
-            .toolbar{
-                Button("Calculate",action: calculateBedtime)
-            }
+            
             
             .alert(alertTitle,isPresented: $showingAlert){
                 Button("OK"){}
@@ -84,34 +136,10 @@ struct ContentView: View {
                 Text(alertMessage)
             }
         }
+        
     }
     
-    func calculateBedtime() {
-        
-        do{
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-            
-        }catch{
-            
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
-            
-        }
-        
-        showingAlert = true
-    }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
